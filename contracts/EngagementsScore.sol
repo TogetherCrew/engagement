b9f30bf3b5fa292b4eaff57ee22121feb20e13cd
zuies <zurabichkhikvadze66@gmail.com>
2024-05-27 17:42:40 +0300

    initial commit

---
+++ contracts/EngagementsScore.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+// Uncomment this line to use console.log
+// import "hardhat/console.sol";
+
+contract EngagementsScore {
+    constructor() {
+        
+    }
+}
