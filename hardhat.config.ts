66319f6a27b6f384cc49fe79092607e24aa817b9
zuies <zurabichkhikvadze66@gmail.com>
2024-06-07 14:53:53 +0300

    close #48

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,23 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
+require("dotenv").config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: process.env.ALCHEMY_SEPOLIA_ENDPOINT,
+      accounts: [process.env.PRIVATE_KEY!],
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545/",
+      chainId: 31337,
+    },
+  },
+  etherscan: {
+    // Obtain one at https://etherscan.io/myapikey
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
